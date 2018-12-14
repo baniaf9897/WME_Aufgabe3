49535fec6f76186e924efd1d3ee67c4efb3ba091
baniaf9897 <fabian9897@gmail.com>
2018-12-14 21:18:56 +0100

    adding comments

--- app.js
+++ app.js
@@ -45,12 +45,15 @@ var server = app.listen(3000, function () {
 
 
 app.get('/items', function(req,res){
+    //send all entries from json
     res.send(json);
 });
 app.get('/items/:id',function(req,res){
+    //read id from url
     var id = Number(req.params.id);
+    //filter json for country with same id
     var country = json.filter((el) => el.id == parseId(id))[0];
-
+    //error handling | send correct country
     if(country == undefined)
         res.status(400).send("No such id " + id + " in database.");
     else
@@ -58,13 +61,14 @@ app.get('/items/:id',function(req,res){
     
 });
 app.get('/items/:id1/:id2',function(req,res){
+    //read ids from url
     let id1 = Number(req.params.id1);
     let id2 = Number(req.params.id2);
-
+    // calculate range
     var range = id2 - id1;
-
+    
     var answer = [];
-
+    //error handling | filter json for countries, which are within the range
     if(range < 0 | id2 >= json.length | id1 <= 0)
         res.status(400).send("Range not possible.");
     else{
@@ -76,7 +80,8 @@ app.get('/items/:id1/:id2',function(req,res){
         res.send(answer);
     }
 });
-app.get('/properties',function(req,res){    
+app.get('/properties',function(req,res){
+     //filter json for object with maximum amount of properties  
     var max = json.reduce((max, key) => {
         if(key.length > max.length)
             return key
@@ -84,42 +89,49 @@ app.get('/properties',function(req,res){
             return max
         , json[0]
     });
+    //send all keys of this object
     res.send(Object.keys(max));
 });
 app.get('/properties/:num',function(req,res){
+    //get id from url
     var num = Number(req.params.num) - 1;
-    var keys = json.map((row) => Object.keys(row)).reduce((max, key) => {
-        if(key > max)
+    //filter json for object with maximum amount of properties
+    var max = json.reduce((max, key) => {
+        if(key.length > max.length)
             return key
         else
             return max
         , json[0]
     });
-
+    //error handling
     if(num < 0 || num >= keys.length)
         res.status(400).send("No such property available.")
-
-    res.send(keys[num]);
+    //send correct property
+    res.send(Object.keys(max[num]));
 });
 
 app.post('/items',function(req,res){
-    console.log("BODY",req.body);
+    //read data from request
     var country = req.body;
+    //create correct id format
     country.id = parseId(json.length + 1);
+    //add new country into array
     json.push(country);
     res.send("Added country"+ country.name +"to list!");
 });
 
 app.delete('/items',function(req,res){
+    //deletes last index in json and returns deleted country
     var country = json.pop();
     res.send("Deleted last country " + country.name + " !");
 });
 
 app.delete('/items/:id',function(req,res){
+    //first get id from url
     var id = req.params.id;
-
+    //filter json for country with same ID
     var country = json.filter((el) => el.id == parseId(id))[0];
-    console.log(country);
+    //if country exist => delete it from array, otherwise return with status 400
     if(country == undefined)
         res.status(400).send("No such id "+id+" in database");
     else{
@@ -129,8 +141,7 @@ app.delete('/items/:id',function(req,res){
     }
 });
 
-
-
+//helper function for converting number into ID format
 function parseId(id){
     return ("00" + id).slice(-3);
 }
