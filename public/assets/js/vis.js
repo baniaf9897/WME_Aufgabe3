e831eb37989aefd0a4002d1764ae7ef9b0cb8930
baniaf9897 <fabian9897@gmail.com>
2018-12-31 15:08:10 +0100

    adding second graph and selection + make them independent

--- public/assets/js/vis.js
+++ public/assets/js/vis.js
@@ -1,13 +1,17 @@
 var props;
 var data;
 var width = 600;
-var height = 300;
+var height = 200;
 
-var svg 
-var chart
+var chart1
+var chart2
 
 var yScale
 var xScale
+
+var xAxis
+var yAxis
+
 $.ajax({
     type: "GET",
     dateType: "json",
@@ -32,26 +36,27 @@ function drawCharts(){
     
     yScale = d3.scaleLinear()
     .range([height, 0])
-    .domain([0, 3000]);
+    .domain([0, d3.max(data.map((item) => item.id))]);
     
     xScale = d3.scaleBand()
     .range([0, width])
     .domain(data.map((country) => country.name))
     .padding(0.4);
     
-    
+    xAxis = d3.axisBottom(xScale);
+    yAxis = d3.axisLeft(yScale);
 
-    svg = d3.select("svg").attr('height',height + 150).attr('width',width + 100);
-    chart = svg.append('g').attr('transform', 'translate(50,50)');
+    var svg1 = d3.select("#chart1").append("svg").attr('height',height + 150).attr('width',width + 100);
+    chart1 = svg1.append('g').attr('transform', 'translate(50,50)');
 
-    chart.append('g').call(d3.axisLeft(yScale));
+    chart1.append('g').classed('y',true).call(yAxis);
    
-    chart.append('g').attr('transform', 'translate(0, '+ height +')')
-    .call(d3.axisBottom(xScale)).selectAll("text")
+    chart1.append('g').classed('x',true).attr('transform', 'translate(0, '+ height +')')
+    .call(xAxis).selectAll("text")
     .attr("transform", "rotate(90)").attr("dy", ".35em")
     .attr("x", 15).attr("y",0).style("text-anchor", "start");
    
-    chart.selectAll()
+    chart1.selectAll()
     .data(data)
     .enter()
     .append('rect')
@@ -59,37 +64,85 @@ function drawCharts(){
     .attr('y', (s) => yScale(s.id))
     .attr('height', (s) => height - yScale(s.id))
     .attr('width', xScale.bandwidth())
+    .attr('fill', '#333333')
+
+    var svg2 = d3.select("#chart2").append("svg").attr('height',height + 150).attr('width',width + 100);
+    chart2 = svg2.append('g').attr('transform', 'translate(50,50)');
+
+    chart2.append('g').classed('y',true).call(yAxis);
+   
+    chart2.append('g').classed('x',true).attr('transform', 'translate(0, '+ height +')')
+    .call(xAxis).selectAll("text")
+    .attr("transform", "rotate(90)").attr("dy", ".35em")
+    .attr("x", 15).attr("y",0).style("text-anchor", "start");
+   
+    chart2.selectAll()
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', (s) => xScale(s.name))
+    .attr('y', (s) => yScale(s.id))
+    .attr('height', (s) => height - yScale(s.id))
+    .attr('width', xScale.bandwidth())
+    .attr('fill', '#333333')
 
 }
 
 function drawDropDowns(){
     
-    var dropDown = d3.select('select')
-    .on('change',selectProperty)
+    var dropDown1 = d3.selectAll('#select1').append('select')
+    .on('change',selectProperty1)
 
-    var options = dropDown
+    dropDown1
     .selectAll('option')
 	.data(props).enter()
 	.append('option')
     .text(function (d) { return d; });
+
+    var dropDown2 = d3.selectAll('#select2').append('select')
+    .on('change',selectProperty2)
+
+    dropDown2
+    .selectAll('option')
+	.data(props).enter()
+	.append('option')
+    .text(function (d) { return d; });
+    
 }
 
-function selectProperty(){
-    var selectedProperty = d3.select('select').property("value");
-    updateCharts(selectedProperty);
+function selectProperty1(){
+    var selectedProperty = d3.select('#select1').select('select').property("value");
+    updateCharts(selectedProperty,chart1);
+}
+function selectProperty2(){
+    var selectedProperty = d3.select('#select2').select('select').property("value");
+    updateCharts(selectedProperty,chart2);
 }
 
 
-function updateCharts(prop){
 
+function updateCharts(prop,_chart){
 
-    chart.selectAll('rect')
+    yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, d3.max(data.map((item) => item[prop]))]);
+    
+    yAxis = d3.axisLeft(yScale);
+
+    _chart.select('.y').call(yAxis);
+
+    _chart.selectAll('rect')
     .data(data)
     .attr('x', (s) => xScale(s.name))
     .attr('y', (s) => yScale(s[prop]))
-    .attr('height', (s) => height - yScale(s.id))
+    .attr('height', 
+        (s) =>{
+            if(height - Math.round(yScale(s[prop])) < 0)
+                return 0;
+            else
+                return height - Math.round(yScale(s[prop]))
+        }
+        )
     .attr('width', xScale.bandwidth())
-
-
 }
 
